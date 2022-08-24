04a0aa524f79fcc251b55ac2b58e8dd3ba190eb9
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-08-24 14:27:46 +1000

    fixes

--- Lab 05 - Troubleshooting Guest Operating System Networking/Jumpbox2.sh
+++ Lab 05 - Troubleshooting Guest Operating System Networking/Jumpbox2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-sudo apt update
+sudo apt update > /dev/null
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
 sudo apt install xfce4-session
-sudo apt-get -y install 
+sudo apt-get -y install xrdp
 sudo systemctl enable xrdp
 sudo adduser xrdp ssl-cert
 echo xfce4-session >~/.xsession
 sudo service xrdp restart
 sudo apt install firefox -y
-xfconf-query -c xfwm4 -p /general/use_compositing -t bool -s false
+xfconf-query -c xfwm4 -p /general/use_compositing -t bool -s false --create
