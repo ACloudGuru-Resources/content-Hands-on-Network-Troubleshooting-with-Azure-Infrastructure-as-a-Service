c0858902179c1ec2651dcb3bdb5d8bd620639ea8
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-08-29 08:29:24 +1000

    fixes

--- Shared/Remove-Deployments.ps1
+++ Shared/Remove-Deployments.ps1
@@ -1,4 +1,2 @@
-Install-PackageProvider -Name ‘Nuget’ -MinimumVersion 2.8.5.201 -Force
-Install-Module 'Az.Accounts','Az.Compute','Az.Resources' -Force
 Connect-AzAccount -Identity
 Get-AzResourceGroupDeployment (Get-AzResourceGroup).ResourceGroupName | Remove-AzResourceGroupDeployment -ErrorAction SilentlyContinue
--- Shared/Set-ScheduledTasks.ps1
+++ Shared/Set-ScheduledTasks.ps1
@@ -1,7 +1,5 @@
 # Remove-Deployments
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File C:\Temp\Remove-Deployments.ps1"
-$Trigger = New-ScheduledTaskTrigger -At (Get-Date).AddMinutes(3) -RepetitionInterval (New-TimeSpan -Minutes 1) 
+$Trigger = New-ScheduledTaskTrigger -At (Get-Date).AddMinutes(3) -RepetitionInterval (New-TimeSpan -Minutes 1) -Once 
 Register-ScheduledTask -TaskName "Remove-Deployments" -Action $Action -Trigger $Trigger -Description "Clean-up Azure Deployments"
 Start-ScheduledTask -TaskName "Remove-Deployments"
-
-# 
