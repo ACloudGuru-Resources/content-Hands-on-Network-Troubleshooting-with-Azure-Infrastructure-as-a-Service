98bde18867b2c479b221604cb99146bd069f693e
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-09-05 10:59:45 +1000

    updates

--- Lab 01 - Troubleshooting Network Security Groups/Initialize-FileServer1.ps1
+++ Lab 01 - Troubleshooting Network Security Groups/Initialize-FileServer1.ps1
@@ -2,6 +2,6 @@ $ProgressPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 Install-WindowsFeature FS-FileServer
 New-Item -Path C:\ -Name Web -ItemType Directory
-New-SmbShare -Path C:\Web -Name Web
+New-SmbShare -Path C:\Web -Name Web -FullAccess Everyone
 Invoke-WebRequest -Uri 'https://github.com/ACloudGuru-Resources/content-Hands-on-Network-Troubleshooting-with-Azure-Infrastructure-as-a-Service/raw/master/Lab%2001%20-%20Troubleshooting%20Network%20Security%20Groups/WebApp/WebApp.zip' -OutFile 'C:\temp\WebApp.zip'
 Expand-Archive -Path 'C:\temp\WebApp.zip' -DestinationPath 'C:\Web' -Force
