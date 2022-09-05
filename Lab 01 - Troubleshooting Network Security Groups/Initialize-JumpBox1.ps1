195a42653be2983783da09a1e9d1c18f6bbadf71
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-09-05 11:19:24 +1000

    updates

--- Lab 01 - Troubleshooting Network Security Groups/Initialize-JumpBox1.ps1
+++ Lab 01 - Troubleshooting Network Security Groups/Initialize-JumpBox1.ps1
@@ -4,14 +4,14 @@ Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideClock" -Value 1
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft" -Name "Edge"
+# Cleanup Edge
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft" -Name "Edge" -ErrorAction "SilentlyContinue"
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name 'HideFirstRunExperience' -Value 1
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "HomepageLocation" -Value "http://10.0.1.80"
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "RestoreOnStartup" -Value 4 -Type "DWORD"
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "Recommended"
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\Recommended\RestoreOnStartupURLs" -Name "1" -Value "http://10.0.1.80"
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "RestoreOnStartupURLs"
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\RestoreOnStartupURLs" -Name "1" -Value "http://10.0.1.80"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "HomepageLocation" -Value "http://10.0.1.80"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "HomepageIsNewTabPage" -Value "1" -Type "DWORD"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "NewTabPageQuickLinksEnabled" -Value "0" -Type "DWORD"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge" -Name "NewTabPageAllowedBackgroundTypes" -Value "0" -Type "DWORD"
 
 #Set Scheduled Tasks
 # Deploy Virus
