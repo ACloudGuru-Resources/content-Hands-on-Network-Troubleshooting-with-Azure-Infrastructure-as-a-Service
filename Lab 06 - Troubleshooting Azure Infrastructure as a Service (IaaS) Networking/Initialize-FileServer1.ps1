bffd3cc29303a577d551d13381c74120f96891b2
Wayne Hoggett <wayne.hoggett@acloud.guru>
2022-09-12 14:34:03 +1000

    updates

--- Lab 06 - Troubleshooting Azure Infrastructure as a Service (IaaS) Networking/Initialize-FileServer1.ps1
+++ Lab 06 - Troubleshooting Azure Infrastructure as a Service (IaaS) Networking/Initialize-FileServer1.ps1
@@ -3,7 +3,7 @@ $WarningPreference = "SilentlyContinue"
 Install-WindowsFeature FS-FileServer
 New-Item -Path C:\ -Name Web -ItemType Directory
 New-SmbShare -Path C:\Web -Name Web
-Invoke-WebRequest -Uri 'https://github.com/ACloudGuru-Resources/content-Hands-on-Network-Troubleshooting-with-Azure-Infrastructure-as-a-Service/raw/master/Lab%2001%20-%20Troubleshooting%20Network%20Security%20Groups/WebApp/WebApp.zip' -OutFile 'C:\temp\WebApp.zip'
+Invoke-WebRequest -Uri 'https://github.com/ACloudGuru-Resources/content-Hands-on-Network-Troubleshooting-with-Azure-Infrastructure-as-a-Service/raw/master/Lab%2006%20-%20Troubleshooting%20Azure%20Infrastructure%20as%20a%20Service%20(IaaS)%20Networking/WebApp/WebApp.zip' -OutFile 'C:\temp\WebApp.zip'
 Expand-Archive -Path 'C:\temp\WebApp.zip' -DestinationPath 'C:\Web' -Force
 
 #Install Modules
